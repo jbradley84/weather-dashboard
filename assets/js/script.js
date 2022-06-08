c3a3557dca46930baf846541f64a43594eb6b750
jbradley84 <utbradley84@yahoo.com>
2022-06-08 17:47:37 -0500

    built out apiUrl function to make second api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var formSubmitHandler = function(event) {
     event.preventDefault();
 
     // get value from input element
-    var city = city.value.trim();
+    var city = cityInputEl.value.trim();
 
     if (city) {
         getWeatherInfo(city);
@@ -20,15 +20,40 @@ var formSubmitHandler = function(event) {
 
 var getWeatherInfo = function(location) {
     // format the OpenWeather API url
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=1&appid=2c906a4a85e01e7cd8e82db1002bb787";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=2c906a4a85e01e7cd8e82db1002bb787";
 
     // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-            console.log(response);
+            //console.log(response);
+            response.json().then(function(data) {
+                var cityName = data.name;
+                var latEl = data.coord.lat;
+                var lonEl = data.coord.lon
+
+                // one call API 1.0
+                var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latEl + "&lon=" + lonEl + "&exclude=minutely,hourly,alerts&units=imperial&appid=2c906a4a85e01e7cd8e82db1002bb787";
+
+                // make a get request to url
+                fetch(oneCallUrl).then(function(response) {
+                    // request was successful
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                            displayWeather(data, cityName);
+                            displayFiveDay(data);
+                            saveSearch(cityName, oneCallUrl);
+                        })
+                    }
+                })
+            })
         }
     })
 }
+
+//var displayWeather = function() {
+
+
+//}
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
